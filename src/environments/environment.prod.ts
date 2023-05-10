913fe2e63112226ce226406ffc29f1c760756901
John Balasis <cs2180008@di.uoa.gr>
2023-05-10 16:21:20 +0300

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  beta: false,
+  API_LOGIN: '/api/oauth2/authorization/openaire',
+  API_ENDPOINT: '/api',
+  WS_ENDPOINT: '/api/websocket',
+  STATS_API_ENDPOINT: '/stats/api/',
+  projectName: 'CountryPages',
+  profileName: 'country-pages',
+  OSO_STATS_API_ENDPOINT: 'https://services.openaire.eu/stats-tool/',
+  osoStatsProfileName: 'country-pages'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  beta: false,
+  API_LOGIN: 'http://localhost:8280/observatory/login',
+  API_ENDPOINT: '/observatory',
+  STATS_API_ENDPOINT: 'http://dl123.madgik.di.uoa.gr/stats/api/',
+  WS_ENDPOINT: 'http://localhost:8280/observatory',
+  // STATS_API_ENDPOINT: 'https://stats.madgik.di.uoa.gr/stats-api/',
+  projectName: 'Observatory',
+  profileName: 'eosc-obs',
+  OSO_STATS_API_ENDPOINT: 'https://services.openaire.eu/stats-tool/',
+  osoStatsProfileName: 'observatory'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
