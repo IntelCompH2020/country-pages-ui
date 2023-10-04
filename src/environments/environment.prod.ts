3a8d74f1deffe517c8e8b055e2ff227fedccdaa0
TheQuaker <cs2180008@di.uoa.gr>
2023-10-04 16:46:24 +0300

    enviroment prod changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   beta: false,
-  API_LOGIN: '/api/oauth2/authorization/openaire',
+  API_LOGIN: '/api/oauth2/authorization/bsc',
   API_ENDPOINT: '/api',
   WS_ENDPOINT: '/api/websocket',
   STATS_API_ENDPOINT: '/stats/api/',
-  projectName: 'CountryPages',
-  profileName: 'country-pages',
+  projectName: 'Intelcomp survey tool',
+  profileName: 'intelcomp-survey-tool',
   OSO_STATS_API_ENDPOINT: 'https://services.openaire.eu/stats-tool/',
   osoStatsProfileName: 'country-pages'
 };
