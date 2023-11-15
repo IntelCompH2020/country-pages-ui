139e7e568cb7a269b8d18061d67c995c58e87c50
jBalasis <j.balasis92@gmail.com>
2023-11-15 12:28:35 +0200

    added home page title and dashboard home page title and text

--- src/app/pages/contribution-dashboard-extension/home/contributions-home-extension.component.ts
+++ src/app/pages/contribution-dashboard-extension/home/contributions-home-extension.component.ts
@@ -10,7 +10,9 @@ import {
 
 export class ContributionsHomeExtensionComponent extends ContributionsHomeComponent {
 
-  title: string = 'Intelcomp Survey Title';
-  text: string = 'Description';
+  title: string = 'Welcome to your personalized section of the IntelComp Policy Participation Portal!';
+  text: string = '<p>Here, you can effortlessly complete your assigned surveys, gaining valuable insights and contributing to important discussions.</p>' +
+    '<p>Moreover, you have the ability to assign portions of the survey to selected contributors. This collaborative approach ensures comprehensive coverage of topics, leveraging the expertise of various individuals to enrich the overall survey quality.</p>' +
+    '<p>Stay informed, collaborate, and contribute to meaningful surveys! Start by selecting a survey on the left.</p>';
 
 }
