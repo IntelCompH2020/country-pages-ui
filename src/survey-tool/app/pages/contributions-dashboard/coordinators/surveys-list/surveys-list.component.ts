4a09f5b28a86ff2174b1337b58f084f6664c4c65
jBalasis <j.balasis92@gmail.com>
2023-12-18 11:51:10 +0200

    fix for side menu and content loading when navigating directly with url part 2

--- src/survey-tool/app/pages/contributions-dashboard/coordinators/surveys-list/surveys-list.component.ts
+++ src/survey-tool/app/pages/contributions-dashboard/coordinators/surveys-list/surveys-list.component.ts
@@ -5,10 +5,13 @@ import {UserService} from "../../../../services/user.service";
 import {SurveyService} from "../../../../services/survey.service";
 import {Subscriber} from "rxjs";
 import {Coordinator} from "../../../../domain/userInfo";
+import { StakeholdersService } from "../../../../services/stakeholders.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-survey-lists',
-  templateUrl: 'surveys-list.component.html'
+  templateUrl: 'surveys-list.component.html',
+  providers: [SurveyService, StakeholdersService]
 })
 
 export class SurveysListComponent implements OnInit{
@@ -17,12 +20,30 @@ export class SurveysListComponent implements OnInit{
   coordinator: Coordinator = null;
   surveys: Paging<Model> = null;
 
-  constructor(private surveyService: SurveyService) {
-  }
+  constructor(private surveyService: SurveyService, private stakeholderService: StakeholdersService,
+              private route: ActivatedRoute, private userService: UserService) {}
 
   ngOnInit() {
     this.coordinator = JSON.parse(sessionStorage.getItem('currentCoordinator'));
+    if (!this.coordinator) {
+      this.getCoordinator();
+    } else {
+      this.getSurveys();
+    }
+  }
 
+  getCoordinator() {
+    this.route.snapshot.paramMap.get('id')
+    this.stakeholderService.getCoordinatorById(this.route.snapshot.paramMap.get('id')).subscribe({
+      next: value => this.coordinator = value,
+      error: err => console.error(err),
+      complete: () => this.getSurveys()
+    });
+  }
+
+  getSurveys() {
+    console.log(this.coordinator);
+    this.userService.changeCurrentCoordinator(this.coordinator);
     if (this.coordinator?.type) {
       this.subscriptions.push(
         this.surveyService.getSurveys('type', this.coordinator.type).subscribe(
@@ -31,7 +52,6 @@ export class SurveysListComponent implements OnInit{
         )
       );
     }
-
   }
 
   ngOnDestroy() {
--- src/survey-tool/app/pages/contributions-dashboard/my-surveys/my-surveys.component.ts
+++ src/survey-tool/app/pages/contributions-dashboard/my-surveys/my-surveys.component.ts
@@ -68,9 +68,6 @@ export class MySurveysComponent implements OnInit, OnDestroy{
       }
     });
 
-
-
-
   }
 
   ngOnDestroy() {
